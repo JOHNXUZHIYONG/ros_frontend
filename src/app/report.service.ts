8a8d914b06dbf227b0499e135aca56e15bf14fb7
XuZhiyong <xu_zhiyong@artc.a-star.edu.sg>
2024-06-08 22:21:58 +0800

    add http function

--- src/app/report.service.ts
+++ src/app/report.service.ts
@@ -1,6 +1,7 @@
+import { retry } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,31 +12,72 @@ export class ReportService {
 
   private apiUrl = 'http://127.0.0.1:8000';
 
-  get(){
+  get() {
     return this.http.get(this.apiUrl)
   }
-  get_item(id:number){
+
+  // item
+  get_item(id: number) {
     const url = `${this.apiUrl}/get_item/${id}`
-    return this.http.get(url)
+    return this.http.get(url).pipe(retry(1),catchError(this.handleError))
   }
 
-  add_item(body:any){
+  add_item(body: any) {
     const url = `${this.apiUrl}/add_item`
     return this.http.post(url, body)
   }
 
-  delete_item(id:number){
+  delete_item(id: number) {
     const url = `${this.apiUrl}/delete_item/${id}`
     return this.http.delete(url)
   }
 
-  update_item(id:number, body:any){
+  update_item(id: number, body: any) {
     const url = `${this.apiUrl}/update_item/${id}`
-    return this.http.put(url, body)
+    return this.http.put(url, body).pipe(retry(3), catchError(this.handleError))
+  }
+
+  // user
+  get_user(id: number) {
+    const url = `${this.apiUrl}/get_user/${id}`
+    return this.http.get(url).pipe(retry(3), catchError(this.handleError))
+  }
+
+  add_user(body:any){
+    const url = `${this.apiUrl}/add_user`
+    return this.http.post(url, body).pipe(retry(1), catchError(this.handleError))
+  }
+
+  delete_user(id:number){
+    const url = `${this.apiUrl}/delete_user/${id}`
+    return this.http.delete(url).pipe(retry(1), catchError(this.handleError))
+  }
+
+  update_user(id:number, body:any){
+    const url = `${this.apiUrl}/update_user/${id}`
+    return this.http.put(url,body).pipe(retry(1), catchError(this.handleError))
   }
 
 
-  get_report(id:number){
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 404) {
+      console.error("Resource is not found:", error.message);
+      return throwError("Resource is not found ....");
+    } 
+    else if (error.status === 0){
+      console.error("Internet connection is not good", error.message);
+      return throwError("Internet connection is not good...")
+    }
+
+    else {
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+      return throwError( `Backend returned code ${error.status}`)
+    }
+  }
+
+
+  get_report(id: number) {
     const url = `${this.apiUrl}/get_pdf/${id}`
     return this.http.get(url, { responseType: 'blob' })
   }
--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -12,6 +12,7 @@ throw new Error('Method not implemented.');
 }
 
   item_id: number = 1;
+  get_item_response :any;
   
   add_item_body: {"name":string,"price":number} = {"name":"john","price":10};
   add_item_response:any;
@@ -23,12 +24,25 @@ throw new Error('Method not implemented.');
   update_item_body:any;
   update_item_response:any;
 
+  get_user_id: number = 1;
+  get_user_response: any;
+
+  add_user_body: {"name":string, "email":string} = {"name":"john", "email":"john@email"};
+  add_user_response: any;
+
+  delete_user_id: number = 1;
+  delete_user_response:any;
+
+  update_user_id: number = 1;
+  update_user_body:any;
+  update_user_response:any;
+
   pdf_id: number = 1;
   report: any;
   item: any;
-  body = {"name": "john",
-    "price" : 1
-  };
+  // body = {"name": "john",
+  //   "price" : 1
+  // };
 
   newReport: any;
   pdf: any;
@@ -47,10 +61,10 @@ throw new Error('Method not implemented.');
   get_item(id:number){
     id = this.item_id;
     this.reportService.get_item(id).subscribe((data) => {
-      this.item = data;
+      this.get_item_response = data;
     },
     (error) => {
-      console.error(error);
+      this.get_item_response=error;
     })
   }
 
@@ -73,9 +87,30 @@ delete_item(id:number){
 update_item(id:number,body:any){
   id = this.update_item_id;
   body = this.update_item_body;
-  this.reportService.update_item(id,body).subscribe((data)=>{this.update_item_response=data},(error)=>{console.error(error);})
+  this.reportService.update_item(id,body).subscribe((data)=>{this.update_item_response=data},(error)=>{this.update_item_response=error;})
 }
 
+get_user(id:number){
+  id = this.get_user_id
+  this.reportService.get_user(id).subscribe((data)=>{this.get_user_response = data;}, (error)=>{this.get_user_response=error;})
+}
+
+add_user(body:any){
+  body = this.add_user_body;
+  this.reportService.add_user(body).subscribe((data)=>{this.add_user_response  =data;}, (error)=>{this.add_user_response=error;})
+}
+
+delete_user(id:number){
+  id = this.delete_user_id;
+  this.reportService.delete_user(id).subscribe((data)=>{this.delete_user_response = data}, (error)=>{this.delete_user_response=error})
+}
+
+update_user(id:number,body:any)
+{
+  id = this.update_user_id;
+  body = this.update_user_body;
+  this.reportService.update_user(id,body).subscribe((data)=>{this.update_user_response=data},(error)=>{this.update_user_response=error})
+}
 
 f(){
   console.log(123)
